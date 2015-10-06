dde3dd70bd2a8b188234d12902a2d4c544470017
nikolojedison <nikolojedison@gmail.com>
2015-10-06 22:26:42 +0000

    Fiiiiiiiiiiiiiixed

--- octobot/subsystems/drivetrain.py
+++ octobot/subsystems/drivetrain.py
@@ -10,20 +10,15 @@ class Drivetrain(Subsystem):
     def __init__(self, robot):
         super().__init__()
         self.robot = robot
-
-        self.nw = wpilib.Talon(0)
-        self.n = wpilib.Talon(1)
-        self.ne = wpilib.Talon(2)
-        #self.Extraverted iNtuiton... no, wait...
-        self.e = wpilib.Talon(3)
-        self.se = wpilib.Talon(4)
-        self.s = wpilib.Talon(5)
-        self.sw = wpilib.Talon(6)
-        self.w = wpilib.Talon(7)
-
-        self.x = 0
-        self.y = 0
-        self.rotation = 0
+	
+	self.drive_diagonal = wpilib.RobotDrive(DriveMotor(0), DriveMotor(2), DriveMotor(4), DriveMotor(6))
+	self.drive_x = wpilib.RobotDrive(DriveMotor(1), DriveMotor(5))
+	self.drive_y = wpilib.RobotDrive(DriveMotor(3), DriveMotor(7))
+	self.drive_diagonal.setInvertedMotor(self.drive_diagonal.MotorType.kFrontRight, True)
+	self.drive_diagonal.setInvertedMotor(self.drive_diagonal.MotorType.kRearRight, True)
+	self.drive_x.setInvertedMotor(self.drive_x.MotorType.kRight, True)
+	self.drive_y.setInvertedMotor(self.drive_y.MotorType.kRight, True)
+	
 
     def initDefaultCommand(self):
         '''When no other command is running, let the operator drive around using the joystick.'''
@@ -32,12 +27,20 @@ class Drivetrain(Subsystem):
     def log(self):
         pass
 
-    def manualSet(self, output):
-        self.nw.set(output)
-        self.n.set(output)
-        self.ne.set(output)
-        self.e.set(output)
-        self.se.set(output)
-        self.s.set(output)
-        self.sw.set(output)
-        self.w.set(output)
+    def driveJoystick(self, joystick):
+	precision = True
+	x = drive_control(joystick.getX(), precision)
+	y = drive_control(joystick.getY(), precision)
+	z = precision_mode(dead_zone(joystick.getRawAxis(3)*.75, .1), precisiou)
+	
+	if x>1:
+		x=1
+	elif x<-1:
+		x=-1
+	self.driveManual(x,y,z)
+	
+    def driveManual(self, x, y, rotation):
+	self.x, self.y, self.rotation = x, y, rotation
+	self.drive_diagonal.mecanumDrive_Cartesian(x, y, rotation, 0)
+	self.drive_x.arcadeDrive(x, rotation)
+	self.drive_y.arcadeDrive(y, rotation)
