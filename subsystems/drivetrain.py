f41990fc5f7120588c775d04e3994f1129acaad7
nikolojedison <nikolojedison@gmail.com>
2015-12-17 22:09:39 +0000

    try that one

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -53,7 +53,7 @@ class Drivetrain(Subsystem):
         self.drive_diagonal.setInvertedMotor(self.drive_diagonal.MotorType.kFrontRight, True)
         self.drive_diagonal.setInvertedMotor(self.drive_diagonal.MotorType.kRearLeft, True)
         self.drive_x.setInvertedMotor(self.drive_x.MotorType.kFrontLeft, True)
-        self.drive_y.setInvertedMotor(self.drive_y.MotorType.kFrontRight, True)
+        self.drive_y.setInvertedMotor(self.drive_y.MotorType.kFrontLeft, True)
 
 	
 
