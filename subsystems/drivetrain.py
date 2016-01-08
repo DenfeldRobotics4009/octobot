7b27a25c0073418453003090806ad9469775ce48
nikolojedison <nikolojedison@gmail.com>
2016-01-08 13:01:11 -0600

    pitch

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -82,7 +82,7 @@ class Drivetrain(Subsystem):
 	    x = drive_control(joystick.getX()*2, precision)
 	    y = drive_control(joystick.getY()*2, precision)
 	    z = precision_mode(dead_zone(joystick.getRawAxis(2)*2, .1), precision)
-	    a = self.gyro.getRoll()
+	    a = self.gyro.getPitch()
 	    self.driveManual(x, y, z, a)
 	    if x>1:
 		    x=1
