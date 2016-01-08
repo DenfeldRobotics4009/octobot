f27539437f802407cfbd901039303a66bceb73f3
nikolojedison <nikolojedison@gmail.com>
2016-01-08 11:00:39 -0600

    Pre-thing commit

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -28,7 +28,12 @@ class Drivetrain(Subsystem):
         super().__init__()
         self.robot = robot
 
-        self.gyroAngle = IMUSimple()
+      #  if robot.isReal():
+       #     self.gyro = IMUSimple()
+       # else:
+       #     self.gyro = GyroDummy()
+
+        self.gyro = GyroDummy()
 
         #Gyro definitions, I think...
         self.x = 0
@@ -85,12 +90,12 @@ class Drivetrain(Subsystem):
 	    elif x<-1:
 		    x=-1
 
-	    self.driveManual(x, y, z)
+	    self.driveManual(x, y, z, a)
 
     #def driveManual(self, x, y, z):
-    def driveManual(self, x, y, rotation, gyroAngle):
-        self.x, self.y, self.rotation = x, y, rotation
-        self.drive_diagonal.mecanumDrive_Cartesian(x, y, rotation, gyroAngle)
+    def driveManual(self, x, y, rotation, a):
+        self.x, self.y, self.rotation, self.a = x, y, rotation, a
+        self.drive_diagonal.mecanumDrive_Cartesian(x, y, rotation, a)
         self.drive_x.arcadeDrive(y, rotation)
         self.drive_y.arcadeDrive(x, rotation)
 
